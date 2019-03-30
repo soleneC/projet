57b3871627cd6bce690bd6b955b22b5f0f4c56d8
SoleneConsten <solene.consten@edu.ece.fr>
2019-03-30 13:58:35 +0100

    widget 4 terminé

--- src/component/Widget4.js
+++ src/component/Widget4.js
@@ -20,8 +20,78 @@ export default class Widget4 extends Component {
 
   render() {
     const { artists } = this.state;
+    const tousmois =[];
+    let mois ='';
+
+   artists.forEach(t => {
+      if (t._id ==1)
+      {
+         mois = 'Janvier';
+         tousmois.push(mois);
+      }
+      if (t._id == 2)
+      {
+         mois = 'Février';
+         tousmois.push(mois);
+      }
+      if (t._id == 3)
+      {
+         mois = 'Mars';
+         tousmois.push(mois);
+      }
+      if (t._id == 4)
+      {
+         mois = 'Avril';
+         tousmois.push(mois);
+      }
+      if (t._id == 5)
+      {
+         mois = 'Mai';
+         tousmois.push(mois);
+      }
+      if (t._id == 6)
+      {
+         mois = 'Juin';
+         tousmois.push(mois);
+      }
+      if (t._id == 7)
+      {
+         mois = 'Juillet';
+         tousmois.push(mois);
+      }
+      if (t._id == 8)
+      {
+         mois = 'Août';
+         tousmois.push(mois);
+      }
+      if (t._id == 9)
+      {
+         mois = 'Septembre';
+         tousmois.push(mois);
+      }
+      if (t._id == 10)
+      {
+         mois = 'Octobre';
+         tousmois.push(mois);
+      }
+      if (t._id == 11)
+      {
+         mois = 'Novembre';
+         tousmois.push(mois);
+      }
+      if (t._id == 12)
+      {
+         mois = 'Décembre';
+         tousmois.push(mois);
+      }
+
+    });
 
     const options = {
+
+      labels: tousmois,
+
+      
      theme: {
       palette: 'palette10',
       monochrome: {
@@ -45,7 +115,6 @@ export default class Widget4 extends Component {
 
           position: 'bottom'
         },
-
       }
     }],
 
@@ -66,6 +135,8 @@ export default class Widget4 extends Component {
 
    legend:{
      position:'bottom',
+     offsetY: -10,
+     floating: true,
      labels: {
       colors: '#919DC4',
       useSeriesColors: false
@@ -75,7 +146,7 @@ export default class Widget4 extends Component {
 };
 
 
-const series = [2,1,1,1];
+const series = artists.map((a) => a.count);
   
 
 
