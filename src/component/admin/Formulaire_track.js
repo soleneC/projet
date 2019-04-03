2058daea1c8726b4da470cabe23e3397f36ec5ca
SoleneConsten <solene.consten@edu.ece.fr>
2019-04-03 20:34:18 +0200

    fin formulaire + ajout track

---
+++ src/component/admin/Formulaire_track.js
@@ -0,0 +1,139 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import axios from 'axios';
+
+
+
+class Formulaire_track extends Component {
+
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      fields: {},
+      errors: {}
+    }
+  }
+
+  handleValidation(){
+    let fields = this.state.fields;
+    let errors = {};
+    let formIsValid = true;
+
+    //Name
+    if(!fields["title"]){
+      formIsValid = false;
+      errors["title"] = "Ne peut être vide";
+    }
+
+    //birthday
+    if(!fields["listenings"]){
+      formIsValid = false;
+      errors["listenings"] = "Ne peut être vide";
+    }
+
+    //followers
+    if(!fields["likes"]){
+      formIsValid = false;
+      errors["likes"] = "Ne peut être vide";
+    }
+
+
+    this.setState({errors: errors});
+    return formIsValid;
+  }
+
+  contactSubmit(e){
+    e.preventDefault();
+    if(this.handleValidation()){
+      alert("Ajout de la musique");
+      this.sync();
+    }
+    else{
+      alert("Le formulaire n'est pas complet")
+    }
+
+  }
+
+  handleChange(field, e){
+    let fields = this.state.fields;
+    fields[field] = e.target.value;
+    this.setState({fields});
+  }
+
+
+
+  componentDidMount()
+   {
+  this.sync();
+   }
+
+  render() {
+  return (
+    <Form onSubmit= {this.contactSubmit.bind(this)}>
+    <FormGroup row>
+    <Label for="title" sm={2} class = "label">Nom</Label>
+    <Col sm={10}>
+    <Input type="text" ref="title" id="title" placeholder="Titre" onChange={this.handleChange.bind(this, "title")}
+    value={this.state.fields["title"]}/>
+
+    <span className="error">{this.state.errors["title"]}</span>
+
+    </Col>
+    </FormGroup>
+
+    <FormGroup row>
+    <Label for="duration" sm={2} class = "label">Durée</Label>
+    <Col sm={10}>
+    <Input type="number" ref="duration" id="duration" placeholder="Durée de la musique" onChange={this.handleChange.bind(this, "duration")} 
+    value={this.state.fields["duration"]}/>
+    <span className="error">{this.state.errors["duration"]}</span>
+    </Col>
+    </FormGroup>
+
+    <FormGroup row>
+    <Label for="listenings" sm={2} class = "label">Listenings</Label>
+    <Col sm={10}>
+    <Input type="number" ref="listenings" id="listenings" placeholder="Nombre d'écoutes"  onChange={this.handleChange.bind(this, "listenings")}
+    value={this.state.fields["listenings"]}/>
+    <span className="error">{this.state.errors["listenings"]}</span>
+    </Col>
+    </FormGroup>
+
+    <FormGroup row>
+    <Label for="likes" sm={2} class = "label">Likes</Label>
+    <Col sm={10}>
+    <Input type="number" ref="likes" id="likes" placeholder="Nombre d'écoutes"  onChange={this.handleChange.bind(this, "likes")}
+    value={this.state.fields["likes"]}/>
+    <span className="error">{this.state.errors["likes"]}</span>
+    </Col>
+    </FormGroup>
+
+    <FormGroup check row>
+    <Col sm={{ size: 10, offset: 2 }}>
+    <Button type="submit" className="btn btn-primary">Ajouter</Button>
+    </Col>
+    </FormGroup>
+    </Form>
+    );
+}
+
+      sync() {
+
+         axios.put("http://localhost:8000/tracks", {
+
+
+      title: this.state.fields.title,
+      listenings: this.state.fields.listenings,
+      duration: this.state.fields.duration,
+      likes: this.state.fields.likes
+
+
+        })
+
+      }
+    }
+
+
+    export default Formulaire_track;
--- src/container/Admin.js
+++ src/container/Admin.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import Formulaire_artist from '../component/admin/Formulaire_artist';
 import Formulaire_album from '../component/admin/Formulaire_album';
+import Formulaire_track from '../component/admin/Formulaire_track';
 import List_artist from '../component/admin/List_artist';
 import List_albums from '../component/admin/List_albums';
+import List_tracks from '../component/admin/List_tracks';
 import '../component/admin/Formulaire.css';
 
 class Admin extends Component {
@@ -11,6 +13,7 @@ class Admin extends Component {
     return (
       <Row Class>
         <Col xs="12" sm="6" className = "liste">
+        <h2>Artistes</h2>
           <div><List_artist /></div>
         </Col>
 
@@ -20,6 +23,7 @@ class Admin extends Component {
           </Col>
           
           <Col xs="12" sm="6" className = "liste">
+          <h2>Albums</h2>
           <div><List_albums /></div>
         </Col>
         
@@ -27,6 +31,16 @@ class Admin extends Component {
         <Col xs="12" sm="6" className = "formulaire">
         <h2>Ajouter un Album</h2>
           <div><Formulaire_album /></div>
+        </Col>
+
+        <Col xs="12" sm="6" className = "liste">
+        <h2>Musiques</h2>
+          <div><List_tracks /></div>
+        </Col>
+
+        <Col xs="12" sm="6" className = "formulaire">
+        <h2>Ajouter une musique</h2>
+          <div><Formulaire_track /></div>
         </Col>
         </Row>
 
